d105d6d150eee46b0b4aa555463d56ec22db46d6
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2024-07-31 23:17:39 +0300

    --

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,8 +3,8 @@ import './banner.css'
 const Banner = () => {
   return (
     <div className="banner">
-        <img src="/public/images/banners/shoes.png" alt="shoes" className="banner-img" />
-        <img src="../../../public/images/banners/house.png" alt="house" className="banner-img" />
+        <img src="./shoes.png" alt="shoes" className="banner-img" />
+        <img src="./public/images/banners/house.png" alt="house" className="banner-img" />
     </div>
   )
 }
